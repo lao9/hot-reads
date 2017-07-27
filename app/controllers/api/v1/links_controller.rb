ed8210fc693310943b869eafe615e9e510d61ec1
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-27 15:26:36 -0600

    add post to links api

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::LinksController < ApplicationController
     render json: @links
   end
 
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      render json: @link
+    else
+      render json: @link.errors.full_messages, status: 500
+    end
+  end
+
+  private
+
+    def link_params
+      params.permit(:url)
+    end
+
 end
--- spec/models/link_read_spec.rb
+++ spec/models/link_read_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Link, type: :model do
   describe "validations" do
     it { expect(link).to validate_presence_of(:url) }
   end
+
+  describe "methods" do
+    it "returns the most read article within last 24 hrs" do
+      create_spaced_out_reads
+      hot_reads = Link.hot_reads
+      hot = "http://getbootstrap.com/getting-started/"
+      least_hot = "https://mail.google.com"
+
+      expect(Link.where(url: hot).count).to eq(10)
+      expect(hot_reads.first.url).to eq(hot)
+      expect(Link.where(url: least_hot).count).to eq(1)
+      expect(hot_reads.last.url).to eq(least_hot)
+    end
+  end
 end
