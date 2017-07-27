df85335bb8a5115d30a42d03a2f9dea8feb46675
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-27 15:45:14 -0600

    add reset endpoint for DB

--- spec/requests/api/v1/link_reads/reset_spec.rb
+++ spec/requests/api/v1/link_reads/reset_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 describe "Link Reads Reset API" do
   # for testing on the other endpoint ...
   it "resets database to expected index outputs" do
+    create_list(:link, 40)
 
     get '/api/v1/reset'
-    
+
     expect(response).to be_success
     links = JSON.parse(response.body, symbolize_names: true)
 
